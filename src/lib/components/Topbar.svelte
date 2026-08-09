31a77ff0617614c533058d651fccc83de017cad1
Tarsiux <233294696+Tarsiux@users.noreply.github.com>
2026-08-09 16:42:16 +0200

    Create Topbar component, create home page with basic ui

---
+++ src/lib/components/Topbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<nav>
+  <ol>
+    <li><a href="">Descargar</a></li>
+    <li><a href="">Editar</a></li>
+    <li><a href="">Ajustes</a></li>
+  </ol>
+</nav>
+
+<style>
+  nav {
+    width: 100%;
+    height: 80px;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: red;
+  }
+
+  ol {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: initial;
+  }
+  li {
+    list-style: none;
+    padding: 20px;
+  }
+</style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Home</h1>
+<script lang="ts">
+  import Topbar from "$lib/components/Topbar.svelte";
+
+  let formato = "video";
+</script>
+
+<Topbar />
+
+<input type="text" />
+
+<button type="button" onclick={() => (formato = "video")}>Video</button>
+<button type="button" onclick={() => (formato = "audio")}>Audio</button>
+<button type="submit">Descargar</button>
