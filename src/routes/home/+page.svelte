af4cc4197f20577e0d8d11a8bf68d5fa9b01a43c
Tarsiux <233294696+Tarsiux@users.noreply.github.com>
2026-08-11 11:43:14 +0200

    Functionality to get the path of the selected folder

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
   import Topbar from "$lib/components/Topbar.svelte";
+  import { invoke } from "@tauri-apps/api/core";
 
   let formato = "video";
   let carpeta = "";
+
+  async function select_folder() {
+    try {
+      const res = await invoke<string | null>("select_folder");
+      if (res) {
+        carpeta = res;
+      }
+    } catch (error) {
+      console.error("Error al seleccionar la carpeta:", error);
+    }
+  }
+
+
 </script>
 
 <Topbar />
@@ -11,14 +25,10 @@
 
 <label>
   Seleccionar carpeta
-  <input
-    type="file"
-    webkitdirectory
-    onchange={(e) => (carpeta = e.currentTarget.files?.[0]?.webkitRelativePath.split("/")[0] ?? "")}
-  />
+  <input type="button" on:click={select_folder}/>
 </label>
 <p>Ruta: {carpeta}</p>
 
-<button type="button" onclick={() => (formato = "video")}>Video</button>
-<button type="button" onclick={() => (formato = "audio")}>Audio</button>
+<button type="button" on:click={() => (formato = "video")}>Video</button>
+<button type="button" on:click={() => (formato = "audio")}>Audio</button>
 <button type="submit">Descargar</button>
