7b861f762d21b92e27c4f4acdf1fb5a725ec1b96
Tarsiux <233294696+Tarsiux@users.noreply.github.com>
2026-08-12 20:35:49 +0200

    Declaration of the new Tauri commands

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,8 +1,10 @@
 pub mod folder;
+pub mod download;
 
 pub fn register() -> Box<tauri::ipc::InvokeHandler<tauri::Wry>> {
     Box::new(tauri::generate_handler![
         folder::select_folder,
-
+        download::show_options_video,
+        download::descargar,
     ])
 }
